ef2482b52782345db89c66e4b11f4056e66bd19a
TatianaAV <tvolnova@yandex.ru>
2022-08-22 18:53:06 +0300

    Test

--- src/test/java/ReversePolishNotationCalculatorTest.java
+++ src/test/java/ReversePolishNotationCalculatorTest.java
@@ -7,6 +7,7 @@ import java.util.Deque;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReversePolishNotationCalculatorTest {
+
     private final ReversePolishNotationCalculator test = new ReversePolishNotationCalculator();
 
     @Test
